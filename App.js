397798ee1f42859ad6d80a3f2601c6d94823da5b
BhartiChouhan <95858772+BhartiChouhan@users.noreply.github.com>
2021-12-13 11:53:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Component } from "react";
+import { TouchableOpacity,View,Text,TextInput } from "react-native";
+import  Navigation from './Navigation';
+ 
+function App ()
+{
+  
+  
+    
+      return(
+
+<Navigation />
+
+
+  
+      )
+
+  }
+  export default App;
+
